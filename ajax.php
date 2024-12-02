a188583d084bce7fb23f3c3570863a8d90935707
programimw <programimw@gmail.com>
2024-12-02 10:28:24 +0100

    register user

--- ajax.php
+++ ajax.php
@@ -1,4 +1,104 @@
 <?php
 
+require_once "connect.php";
 
-echo "Test";
+// Register User Data
+if ($_POST["action"] == "register")
+{
+    $name = mysqli_real_escape_string($conn,$_POST['name']);
+    $email = mysqli_real_escape_string($conn,$_POST['email']);
+    $password = mysqli_real_escape_string($conn,$_POST['password']);
+    $password = mysqli_real_escape_string($conn,$_POST['password']);
+    $passwordHashed = password_hash($password, PASSWORD_DEFAULT);;
+    $confirmPassword = mysqli_real_escape_string($conn,$_POST['confirmPassword']);
+    $nameRegex = "/^[a-zA-Z ]{3,20}$/";
+    $passwordRegex = "/^[a-zA-Z0-9-_ ]{4,}$/";
+    //Validimet
+    // Validimi i emrit
+    if (!preg_match($nameRegex, $name)){
+        http_response_code(203);
+        echo json_encode(
+            array(
+                "message" => "Emri vetem karaktere, minimumi 3",
+                "tagError" => "nameError",
+                "tagElement" => "name"
+            ));
+        exit;
+    }
+
+    // Validimi i emailit
+    if (empty($email)){
+        http_response_code(203);
+        echo json_encode(
+            array(
+                "message" => "Email nuk mund te jete bosh",
+                "tagError" => "emailError",
+                "tagElement" => "email"
+            ));
+        exit;
+    }
+
+    // Validimi i password
+    // TODO VALIDATION OF PASSWORD
+
+    // Validojme nese ekziston useri
+    $query_check = "SELECT 
+                           id 
+                    FROM users
+                    WHERE email = '".$email."'  ";
+
+
+    $result_check = mysqli_query($conn, $query_check);
+
+    if (!$result_check){
+        http_response_code(500);
+        echo json_encode(
+            array(
+                "message" => "Internal Server Error",
+                "error" => mysqli_error($conn)
+            ));
+        exit;
+    }
+
+    if (mysqli_num_rows($result_check)){
+        http_response_code(203);
+        echo json_encode(
+            array(
+                "message" => "Ekziston nje user me kete email",
+                "tagError" => "emailError",
+                "tagElement" => "email"
+            ));
+        exit;
+    }
+
+
+    // Ruatja e te dhenave
+    $query_insert = "INSERT INTO users set
+                     name = '".$name."', 
+                     email = '".$email."', 
+                     password = '".$passwordHashed."', 
+                     created_at = '".date("Y-m-d H:i:s")."'
+                       ";
+
+    $resut_insert = mysqli_query($conn, $query_insert);
+
+    if (!$resut_insert){
+        http_response_code(500);
+        echo json_encode(
+            array(
+                "message" => "Internal Server Error",
+                "error" => mysqli_error($conn)
+            ));
+        exit;
+    } else {
+        http_response_code(200);
+        echo json_encode(
+            array(
+                "message" => "Useri u ruajt me sukses",
+            ));
+        exit;
+    }
+
+}
+
+
--- register.php
+++ register.php
@@ -14,7 +14,7 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
     <style>
-        .error-message{
+        .error-message {
             float: left;
             color: #d71b1b
         }
@@ -24,7 +24,7 @@
 
 <body class="gray-bg">
 
-<div class="middle-box text-center loginscreen   animated fadeInDown">
+<div class="middle-box text-center loginscreen  animated fadeInDown">
     <div>
         <!-- Logo -->
         <div>
@@ -34,29 +34,34 @@
         <p>Create account to see it in action.</p>
         <form class="m-t" role="form" action="index.php">
             <div class="form-group">
-                <input type="text" name = "name" id = "name" class="form-control" placeholder="Name" required>
-                <span class = "error-message" id ="nameError"></span>
+                <input type="text" name="name" id="name" class="form-control" placeholder="Name" required>
+                <span class="error-message" id="nameError"></span>
             </div>
             <div class="form-group">
-                <input type="email" name = "email" id = "email"  class="form-control" placeholder="Email" required>
-                <span class = "error-message" id ="emailError"></span>
+                <input type="email" name="email" id="email" class="form-control" placeholder="Email" required>
+                <span class="error-message" id="emailError"></span>
             </div>
             <div class="form-group">
-                <input type="password" name = "password" id = "password" class="form-control" placeholder="Password" required>
-                <span class = "error-message" id ="passwordError"></span>
+                <input type="password" name="password" id="password" class="form-control" placeholder="Password"
+                       required>
+                <span class="error-message" id="passwordError"></span>
             </div>
             <div class="form-group">
-                <input type="password" name = "confirm-password" id = "confirm-password" class="form-control" placeholder="Confirm Password" required>
-                <span class = "error-message" id ="confirmPasswordError"></span>
+                <input type="password" name="confirm-password" id="confirm-password" class="form-control"
+                       placeholder="Confirm Password" required>
+                <span class="error-message" id="confirmPasswordError"></span>
             </div>
             <div class="form-group">
-                <div class="checkbox i-checks"><label> <input type="checkbox"><i></i> Agree the terms and policy </label></div>
+                <div class="checkbox i-checks"><label> <input type="checkbox"><i></i> Agree the terms and policy
+                    </label></div>
             </div>
-            <button type="button" id = 'register' class="btn btn-primary block full-width m-b" onclick="validate()">Register</button>
+            <button type="button" id='register' class="btn btn-primary block full-width m-b" onclick="register_user();">
+                Register
+            </button>
             <p class="text-muted text-center"><small>Already have an account?</small></p>
             <a class="btn btn-sm btn-white btn-block" href="index.php">Login</a>
         </form>
-        <p class="m-t"> <small>Inspinia we app framework base on Bootstrap 3 &copy; 2014</small> </p>
+        <p class="m-t"><small>Inspinia we app framework base on Bootstrap 3 &copy; 2014</small></p>
     </div>
 </div>
 
@@ -66,7 +71,7 @@
 <!-- iCheck -->
 <script src="js/plugins/iCheck/icheck.min.js"></script>
 <script>
-    $(document).ready(function(){
+    $(document).ready(function () {
         $('.i-checks').iCheck({
             checkboxClass: 'icheckbox_square-green',
             radioClass: 'iradio_square-green',
@@ -75,6 +80,12 @@
 </script>
 
 <script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+
+</body>
+
+</html>
+
+
 <script>
 
     toastr.options = {
@@ -94,6 +105,7 @@
         "showMethod": "fadeIn",
         "hideMethod": "fadeOut"
     }
+
     // Display an info toast with no title
 
 
@@ -101,7 +113,7 @@
         return (val == "" || val === undefined || val == null || val === false || val.length <= 0) ? true : false;
     }
 
-    function validate(){
+    function register_user() {
         // var name = document.getElementById("name").value;
         var name = $("#name").val();
         var email = $("#email").val();
@@ -109,72 +121,84 @@
         var confirmPassword = $("#confirm-password").val();
 
         var nameRegex = /^[a-zA-Z ]{3,20}$/;
+        var passwordRegex = /^[a-zA-Z0-9-_ ]{4,}$/;
 
+        var error = 0;
         // validimi i emrit
-        if (!nameRegex.test(name)){
-            $("#name").addClass('error');
-            $("#nameError").text("Emri vetem karaktere, minimumi 3");
-            toastr.error('Emri vetem karaktere, minimumi 3')
-        } else {
-            $("#name").removeClass('error');
-            $("#nameError").text("")
-        }
+        // if (!nameRegex.test(name)) {
+        //     $("#name").addClass('error');
+        //     $("#nameError").text("Emri vetem karaktere, minimumi 3");
+        //     error++;
+        // } else {
+        //     $("#name").removeClass('error');
+        //     $("#nameError").text("")
+        // }
+        //
+        // // validimi i emailit
+        // if (isEmpty(email)) {
+        //     $("#email").addClass('error');
+        //     $("#emailError").text("Email nuk mund te jete bosh");
+        //     error++;
+        // } else {
+        //     $("#email").removeClass('error');
+        //     $("#emailError").text("")
+        // }
+        //
+        // // validimi i passowrdit
+        // if (!passwordRegex.test(password)) {
+        //     $("#password").addClass('error');
+        //     $("#passwordError").text("Password ka minimumi 4 karaktere");
+        //     error++;
+        // } else if (password != confirmPassword) {
+        //     $("#password").addClass('error');
+        //     $("#confirm-password").addClass('error');
+        //     $("#passwordError").text("Passwordet nuk jane te barabarta");
+        //     $("#confirmPasswordError").text("Passwordet nuk jane te barabarta");
+        //     error++;
+        // } else {
+        //     $("#password").removeClass('error');
+        //     $("#confirm-password").removeClass('error');
+        //     $("#passwordError").text("");
+        //     $("#confirmPasswordError").text("");
+        // }
 
-        // validimi i emailit
-        if (isEmpty(email)){
-            $("#email").addClass('error');
-            $("#emailError").text("Email nuk mund te jete bosh");
-            toastr.error('Email nuk mund te jete bosh')
-        } else {
-            $("#email").removeClass('error');
-            $("#emailError").text("")
-        }
-
-        // validimi i passowrdit
-        if (password != confirmPassword){
-            $("#password").addClass('error');
-            $("#confirm-password").addClass('error');
-            $("#passwordError").text("Passwordet nuk jane te barabarta");
-            $("#confirmPasswordError").text("Passwordet nuk jane te barabarta");
-            toastr.error('Passwordet nuk jane te barabarta')
-        } else {
-            $("#password").removeClass('error');
-            $("#confirm-password").removeClass('error');
-            $("#passwordError").text("");
-            $("#confirmPasswordError").text("");
-        }
+        // Data that will be sent in backend
+        var data = new FormData();
+        data.append("action", "register");
+        data.append("name", name);
+        data.append("email", email);
+        data.append("password", password);
+        data.append("confirmPassword", confirmPassword);
 
         // Thirrje ne backend dhe dergimi i te dhenave.
+        if (error == 0) {
+            $.ajax({
+                type: "POST",
+                url: "ajax.php",
+                // dataType: 'json',
+                async: false,
+                cache: false,
+                processData: false,
+                data: data,
+                contentType: false,
+                success: function (response, status, call) {
+                    response = JSON.parse(response);
+                    console.log(response);
 
-        $.ajax({
-            type: "POST",
-            url: "ajax.php",
-            // dataType: 'json',
-            async: false,
-            cache: false,
-            processData: false,
-            data: {
-                "name": name
-            },
-            contentType: false,
-            success: function (response, status, call) {
-                console.log(response)
-                // response = JSON.parse(response);
-                //
-                // if (call.status == 200) {
-                //     window.location.href = "profile.php";
-                // } else {
-                //     $("#" + response.tag).text(response.message);
-                //     Swal.fire('Error', response.message, 'error')
-                // }
-            }
-        })
+                    if (call.status == 200) {
+                        toastr.success(response.message)
+                        setTimeout(function (){
+                            window.location.href = "login.php";
+                        }, 2500);
+
+                    } else {
+                        $("#" + response.tagError).text(response.message);
+                        $("#" + response.tagElement).addClass('error');
+                        // Swal.fire('Error', response.message, 'error')
+                    }
+                },
+            })
+        }
 
     }
-
-
-    console.log(name);
 </script>
-</body>
-
-</html>
